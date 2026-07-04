ae442227b0f48030ebccb4bcf1a9c1e6a2027221
hktodorova <hktodorova@gmail.com>
2026-07-04 18:19:33 +0300

    fix(database): correct database bootstrap script

--- database/01_create_database.sql
+++ database/01_create_database.sql
@@ -1,15 +1,7 @@
-/*
-    Enterprise Data Platform - SQL Server
-    Database bootstrap script
-
-    This script is intentionally small. It creates the database used by the
-    local development environment. Objects are created in the next scripts.
-*/
-
 IF DB_ID(N'EnterpriseDataPlatform') IS NULL
 BEGIN
     CREATE DATABASE EnterpriseDataPlatform;
-END;
+END
 GO
 
 ALTER DATABASE EnterpriseDataPlatform SET RECOVERY FULL;
